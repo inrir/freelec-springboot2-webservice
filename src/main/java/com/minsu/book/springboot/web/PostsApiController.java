92f8b312ac343449c8e5ec6f6e67b139d573c4aa
inrir <kimms5617@naver.com>
2023-03-10 21:16:08 +0900

    [ADD] 수정 등록 기능 추가

--- src/main/java/com/minsu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/minsu/book/springboot/web/PostsApiController.java
@@ -2,19 +2,32 @@ package com.minsu.book.springboot.web;
 
 
 import com.minsu.book.springboot.service.posts.PostsService;
+import com.minsu.book.springboot.web.dto.PostsResponseDto;
 import com.minsu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.minsu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
 
+    // 등록 기능 생성
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
+    // 수정/조회 기능 생성
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
 }
+
---
+++ src/main/java/com/minsu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.minsu.book.springboot.web.dto;
+
+import com.minsu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
